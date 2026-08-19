4969f7cfd449dbf20d5a689442543f06f52d0797
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-08-19 13:36:53 +0300

    chore: improve dashboard layout

--- resources/views/blade/dashboard.blade.php
+++ resources/views/blade/dashboard.blade.php
@@ -5,7 +5,7 @@
         </div>
     </header>
 
-    <div class="py-12">
+    <div class="py-12 my-6">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
